e0893a34556fcb529d5f2931ffa86f8e1b50c6aa
Michael <michael.martynov03@gmail.com>
2024-06-12 15:58:08 +0200

    add dynamic plate input

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -3,6 +3,15 @@ from tkinter import *
 main = Tk()
 text1 = Label(main,text="Tower of Hanoi")
 
+def updateGUI():
+    plates = int(entry_plates.get())
+    thickness = 100
+    height = 50
+
+    for i in range(plates):
+     canvas.create_rectangle(200-thickness+i*10,990-i*50,200+thickness-i*10,990-height-i*50,fill="blue")
+
+
 canvas = Canvas(main,width=1000,height=1000,bg="white")
 
 # Base structure
@@ -11,7 +20,13 @@ canvas.create_rectangle(500,1000,510,400,fill="black")
 canvas.create_rectangle(800,1000,810,400,fill="black")
 canvas.create_rectangle(0,990,1000,1000,fill="black")
 
+text2 = Label(main,text="Please enter amount of plates:")
+entry_plates = Entry(main)
+button = Button(main,text="confirm",command=updateGUI)
 
 text1.pack()
+text2.pack()
+entry_plates.pack()
+button.pack()
 canvas.pack()
 mainloop()
